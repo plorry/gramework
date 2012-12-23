666e0dade329f88005baccc4a7b734b7cce27335
plorry <timebot@gmail.com>
2012-12-23 16:30:23 -0500

    initial commit

---
+++ javascript/config.js
@@ -0,0 +1,13 @@
+var gamejs = require('gamejs');
+
+exports.WIDTH = 256;
+exports.HEIGHT = 224;
+
+exports.SCALE = 2;
+
+exports.scenes = [
+	//0 - ALWAYS victory
+	{"panels":[
+		{'image':'./static/images/backgrounds/background1.png', 'music':'', 'sound':'', 'duration':3000}
+	]}
+];
---
+++ javascript/cutscene.js
@@ -0,0 +1,79 @@
+var config = require('./config');
+var gamejs = require('gamejs');
+
+var Cutscene = exports.Cutscene = function(director, cutsceneId) {
+
+	this.handleEvent = function(event) {
+		if (event.type === gamejs.event.KEY_DOWN || event.type === gamejs.event.MOUSE_DOWN) {
+
+		}
+	};
+
+	this.update = function(msDuration) {
+		elapsed += msDuration;
+		if ((elapsed >= duration) && cutsceneId != TITLE_SCREEN){
+			currentPanel += 1;
+			if (currentPanel >= panels.length) {
+			}
+			elapsed = 0;
+			image = gamejs.image.load(panels[currentPanel].image);
+			if (panels[currentPanel].sound != ''){
+				sound = new gamejs.mixer.Sound(panels[currentPanel].sound);
+				sound.play();
+			}
+			duration = panels[currentPanel].duration;
+
+		//	director.replaceScene(new Cutscene(director, cutsceneId));
+		}
+		if (duration == 'nope'){
+			duration = sound.getLength() * 1000;
+		}
+
+		if (!(duration > 0 && duration < 7000)){
+			duration = sound.getLength() * 1000;
+		}
+
+	};
+
+	this.draw = function(display) {
+		display.blit(image, [0,0], image_rect);
+	};
+
+	this.setLevelDump = function(dump) {
+		try {
+			var cutsceneConfig = JSON.parse(dump);
+			initCutscene(cutsceneConfig);
+		} catch (e) {
+			gamejs.log(e);
+		}
+		return;
+	};
+
+	function initCutscene(cutsceneConfig) {
+		//music = cutsceneConfig.music;
+		panels = [];
+		cutsceneConfig.panels.forEach(function(panel){
+			panels.push(panel);
+		});
+		image = gamejs.image.load(panels[currentPanel].image);
+		if (panels[currentPanel].sound != ''){
+			sound = new gamejs.mixer.Sound(panels[currentPanel].sound);
+			sound.play();
+		}
+		duration = panels[currentPanel].duration;
+		if (duration == null){
+			duration = sound.getLength();
+		}
+		//image = gamejs.image.load('./static/backgrounds/death1b.png');
+		return;
+	};
+
+	var cutsceneId = cutsceneId || 0;
+	var cutsceneConfig = config.scenes[cutsceneId];
+	var elapsed = 0;
+	var currentPanel = 0;
+	var music, image, sound, duration;
+	initCutscene(cutsceneConfig);
+
+	return this;
+}
