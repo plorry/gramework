1f277e034af37413e24bfe376917f34a0bc31f34
Bartek Ciszkowski <bart.ciszk@gmail.com>
2014-01-21 20:38:16 -0500

    More work.

--- demos/runner/main.js
+++ demos/runner/main.js
@@ -3,8 +3,9 @@ var _ = require('underscore'),
     gramework = require('../../../gramework'),
     animate = gramework.animate,
     imgfy = gramework.image.imgfy,
-    Background = gramework.layers.Background,
+    Scrollable = gramework.layers.Scrollable,
     Dispatcher = gramework.Dispatcher,
+    GameController = gramework.input.GameController,
     Entity = gramework.Entity,
     Scene = gramework.Scene,
     Vec2d = gramework.vectors.Vec2d;
@@ -13,15 +14,104 @@ var Player = function(options) {
     Entity.apply(this, arguments);
 
     this.world = options.world;
-    this.sprite = new animate.SpriteSheet('./assets/player.png', 32, 45);
+    this.sprite = new animate.SpriteSheet('./assets/runner.png', 32, 64);
     this.anim = new animate.Animation(this.sprite, "static", {
-        static: {frames: [0, 1, 2, 3, 4], rate: 1.5}
+        static: {frames: [0, 1, 2, 3, 4, 6, 7], rate: 5.5},
+        jump: {frames: [1, 0, 1, 4], rate: 2.5}
     });
+
+    this.controller = GameController({
+        jump: gamejs.event.K_SPACE
+    });
+
+    this.onGround = false;
+    this.isJumping = false;
+    this.velocity = new Vec2d(0, 0);
+
 };
 _.extend(Player.prototype, Entity.prototype, {
+    canMove: function(dx, dy) {
+        var collidedX = false,
+            collidedY = false,
+            start;
+
+        if (dx > 0) {
+            start = this.rect.x;
+            this.rect.x += dx;
+            if (this.world.collides(this)) {
+                console.log("World collides with player.");
+            }
+        } else if (dx < 0) {
+        }
+
+        if (dy > 0) {
+            start = this.rect.y;
+            this.rect.y += dy;
+            if (this.world.collides(this)) {
+                this.rect.y = Math.floor(this.rect.y);
+                while (this.world.collides(this)) {
+                    console.log("Playing colliding on Y");
+                    collidedY = true;
+                    this.rect.y -= 1;
+                }
+            }
+        } else if (dy < 0) {
+            start = this.rect.y;
+            this.rect.y -= dy;
+        }
+
+        return [collidedX, collidedY];
+    },
+
     update: function(dt) {
         this.anim.update(dt);
         this.image = this.anim.update(dt);
+
+        // Adjust dt for vectors.
+        dt = (dt / 1000);
+
+        if (this.onGround && this.isJumping) {
+            gamejs.log("JUMP!");
+            var vec = new Vec2d(0, -20);
+            this.velocity.add(vec.mul(dt));
+        } else {
+            this.velocity.add(this.world.gravity.mul(dt));
+        }
+
+        this.rect.x += this.velocity.x;
+        this.rect.y += this.velocity.y;
+
+        // Decide next movement with a specialty vector.
+        var delta = new Vec2d(0, 0);
+        delta.add(this.velocity).mul(dt);
+
+        // unpacking in ES6 cant come sooner.
+        var collided = this.canMove.apply(this, delta.unpack()),
+            collidedX = collided[0],
+            collidedY = collided[1];
+
+        if (collidedY) {
+            this.velocity.setY(0);
+            if (delta.getY() > 0) {
+                gamejs.log("onGround");
+                this.onGround = true;
+            }
+        } else {
+            if (Math.floor(delta.getY()) !== 0) {
+                //gamejs.log("not onGround");
+                this.onGround = false;
+            }
+        }
+
+        this.isJumping = false;
+    },
+
+    event: function(ev) {
+        var key = this.controller.handle(ev);
+        if (key === this.controller.controls.jump) {
+            this.isJumping = true;
+        }
+
     }
 });
 
@@ -29,9 +119,8 @@ var World = function(options) {
     Scene.apply(this, arguments);
 
     this.game = options.game;
-    this.speed = 10;
     this.player = new Player({
-        x: 0, y: 235,
+        x: 0, y: 135,
         width: 32, height: 45,
         world: this
     });
@@ -39,11 +128,12 @@ var World = function(options) {
     this.accel = 5;
     this.speed = 5;
     this.maxSpeed = 55;
+    this.gravity = new Vec2d(0, 100);
 
     this.layers = [
-        new Background('./assets/bgfar1.png', [1, 0], {speed: 1}),
-        new Background('./assets/bgnear2.png', [-100, 0], {speed: 55}),
-        new Background('./assets/bgnear2.png', [660, 0], {speed: 55}),
+        new Scrollable('./assets/bgfar1.png', [1, 0], {speed: 1}),
+        new Scrollable('./assets/bgnear2.png', [-100, 0], {speed: 55}),
+        new Scrollable('./assets/bgnear2.png', [660, 0], {speed: 55}),
     ];
     this.velocity = new Vec2d(0, 0);
 };
@@ -62,6 +152,18 @@ _.extend(World.prototype, Scene.prototype, {
             layer.speed = (layer.originalSpeed * (this.velocity.magnitude() / (4 * this.speed)));
         }, this);
         return;
+    },
+
+    collides: function(entity) {
+        // Check if the entity is colliding with other entities, or the world
+        // itself.
+        if (entity.rect.y >= (this.height() - entity.rect.height)) {
+            return true;
+        }
+    },
+
+    event: function(ev) {
+        this.player.event(ev);
     }
 });
 
@@ -78,6 +180,10 @@ _.extend(Game.prototype, {
 
     update: function(dt) {
         this.world.update(dt);
+    },
+    
+    event: function(ev) {
+        this.world.event(ev);
     }
 });
 
@@ -100,7 +206,7 @@ var main = function() {
 };
 
 gamejs.preload([
-    './assets/player.png',
+    './assets/runner.png',
     './assets/bgfar1.png',
     './assets/bgnear2.png'
 ]);
