527cfcc1235362acbc9be8327037a97c8b46e73e
Bartek Ciszkowski <bart.ciszk@gmail.com>
2014-01-23 22:24:29 -0500

    refinments, GLOBALS, end screen.

--- demos/runner/hud.js
+++ demos/runner/hud.js
@@ -1,16 +1,24 @@
 var gamejs = require('gamejs'),
-    Font = gamejs.font.Font,
+    gramework = require('../../../gramework'),
+    animate = gramework.animate,
     _ = require('underscore');
 
 var Hud = module.exports = function(position, dimensions, options) {
     gamejs.Rect.apply(this, position, dimensions);
     this.dimensions = dimensions;
     this.world = options.world;
-    this.font = new Font('24px monospace');
+
+    this.numbers = new animate.SpriteSheet('./assets/numbers.png', 60, 80);
 };
 _.extend(Hud.prototype, gamejs.Rect.prototype, {
     draw: function(display) {
-        display.blit(this.font.render("Score: " + this.world.score),
-            [this.dimensions[0] - 140, this.dimensions[1] - 50]);
+        // Figure out the numbers by splitting up the score.
+        var self = this;
+        _.each(String(this.world.score).split(""), function(score, index) {
+            display.blit(self.numbers.get(score), [
+                self.dimensions[0] - (140 - (55 * index)),
+                self.dimensions[1] - 50]
+            );
+        });
     }
 });
--- demos/runner/main.js
+++ demos/runner/main.js
@@ -1,3 +1,4 @@
+/*global window, GLOBALS*/
 var _ = require('underscore'),
     gamejs = require('gamejs'),
     gramework = require('../../../gramework'),
@@ -11,6 +12,13 @@ var _ = require('underscore'),
     Vec2d = gramework.vectors.Vec2d;
 
 var Hud = require('./hud');
+
+// Global variables for the kids to modify
+window.GLOBALS = {
+    jump: -13,
+    speed: 5,
+    maxSpeed: 55
+};
 
 var Coin = function(options) {
     Entity.apply(this, arguments);
@@ -33,8 +41,9 @@ _.extend(Coin.prototype, Entity.prototype, {
     update: function(dt) {
         this.image = this.anim.update(dt);
 
-        this.velocity.setX(-(this.world.velocity.magnitude() / this.maxSpeed));
-        if (this.velocity.magnitude() >= this.maxSpeed) {
+        var setTo = -(this.world.velocity.magnitude() * 0.025 * (GLOBALS.speed + 1));
+        this.velocity.setX(setTo);
+        if (this.velocity.getX() >= this.maxSpeed) {
             this.velocity.truncate(this.maxSpeed);
         }
 
@@ -52,7 +61,7 @@ var CoinEmitter = function(options) {
     this.currentDuration = 0;
     this.duration = this.randomDuration();
 
-    this.endDelay = _.random(5, 10);
+    this.endDelay = _.random(5, 8);
 
 };
 CoinEmitter.prototype = {
@@ -99,6 +108,10 @@ var Player = function(options) {
 
     this.onGround = false;
     this.isJumping = false;
+    this.jumpDelay = 0.6;
+    this.jumpTimer = 0;
+    this.jumpHeight = GLOBALS.jump;
+
     this.velocity = new Vec2d(0, 0);
 
 };
@@ -144,8 +157,13 @@ _.extend(Player.prototype, Entity.prototype, {
         // Adjust dt for vectors.
         dt = (dt / 1000);
 
+        this.jumpTimer -= dt;
+
         if (this.onGround && this.isJumping) {
-            this.velocity.setY(-14);
+            if (this.jumpTimer <= 0) {
+                this.velocity.setY(GLOBALS.jump);
+                this.jumpTimer = this.jumpDelay;
+            }
         }
 
         var vec = new Vec2d().add(this.world.gravity);
@@ -209,6 +227,34 @@ _.extend(Player.prototype, Entity.prototype, {
     }
 });
 
+var EndScreen = function(options) {
+    Scene.apply(this, arguments);
+    this.world = options.world;
+    this.image = imgfy('./assets/suchwin.png');
+    this.numbers = new animate.SpriteSheet('./assets/numbers.png', 60, 80);
+    this.controller = GameController({
+        reset: gamejs.event.K_r
+    });
+};
+_.extend(EndScreen.prototype, Scene.prototype, {
+    draw: function(display) {
+        var self = this;
+        this.view.blit(this.image);
+        _.each(String(this.world.score).split(""), function(score, index) {
+            self.view.blit(self.numbers.get(score), [200 + (55 * index), 110]);
+        });
+        this.camera.draw(this.view, display);
+    },
+    event: function(ev) {
+        if (this.controller.handle(ev) === this.controller.controls.reset) {
+            this.world.totalCoins = 0;
+            this.world.score = 0;
+            this.world.velocity = new Vec2d(0, 0);
+            this.dispatcher.push(this.dispatcher.parent());
+        }
+    }
+});
+
 var World = function(options) {
     Scene.apply(this, arguments);
 
@@ -227,10 +273,14 @@ var World = function(options) {
     this.maxSpeed = 55;
     this.gravity = new Vec2d(0, 50);
 
+    // Total amount of coins in this world before we are done.
+    this.maxCoins = 15;
+    this.totalCoins = 0;
+
     this.layers = [
         new Scrollable('./assets/background.png', [1, 0], {speed: 1}),
-        new Scrollable('./assets/bgnear2.png', [-100, 0], {speed: 55}),
-        new Scrollable('./assets/bgnear2.png', [660, 0], {speed: 55}),
+        new Scrollable('./assets/foreground.png', [-100, 145], {speed: 55}),
+        new Scrollable('./assets/foreground.png', [660, 145], {speed: 55}),
     ];
     this.velocity = new Vec2d(0, 0);
     this.coins = null;
@@ -241,8 +291,8 @@ _.extend(World.prototype, Scene.prototype, {
         dt = (dt / 1000);
 
         var accel = new Vec2d(this.accel, 0);
-        this.velocity.add(accel.mul(dt).mul(this.speed));
-        this.velocity = this.velocity.truncate(this.maxSpeed);
+        this.velocity.add(accel.mul(dt).mul(GLOBALS.speed));
+        this.velocity = this.velocity.truncate(GLOBALS.maxSpeed);
 
         // Keep a CoinEmitter spawned.
         if (this.coins && this.coins.alive) {
@@ -251,15 +301,28 @@ _.extend(World.prototype, Scene.prototype, {
             this.coins = new CoinEmitter({
                 world: this
             });
+            // Add the total to our max coins so we know when to end the level.
+            this.totalCoins += this.coins.count;
         } else if (!this.coins.alive) {
             this.coins = null;
+        }
+
+        if (this.totalCoins >= this.maxCoins) {
+            this.onEnd();
         }
 
         // Send the velocity magnitude to our layers, adjusting their speed
         // based on our velocity
         this.layers.forEach(function(layer) {
-            layer.speed = (layer.originalSpeed * (this.velocity.magnitude() / (4 * this.speed)));
+            layer.speed = (layer.originalSpeed * (this.velocity.magnitude() * 0.005 * (GLOBALS.speed)));
         }, this);
+        return;
+    },
+
+    onEnd: function() {
+        this.game.dispatcher.push(new EndScreen({
+            world: this, width: 800, height: 600
+        }));
         return;
     },
 
@@ -320,9 +383,11 @@ var main = function() {
 };
 
 gamejs.preload([
+    './assets/suchwin.png',
+    './assets/numbers.png',
     './assets/coin.png',
     './assets/runner.png',
     './assets/background.png',
-    './assets/bgnear2.png'
+    './assets/foreground.png',
 ]);
 gamejs.ready(main);
