93c7bbb5e2c2998d783358e1456d2533a4611063
Bartek Ciszkowski <bart.ciszk@gmail.com>
2014-04-10 17:44:13 -0400

    Improve Dispatcher by passing gamejs into it.
    This allows us to bind gamejs onTick and onEvent to the Dispatcher,
    saving a few lines of boiler-plate code that would occur in every game.
    
    If it needs to be changed, we can now extend the Dispatcher class
    similar to how we've done other classes.
    
    Additionally wrote some tests to cover this change as well as some previous
    ones.

--- tests/dispatcher.js
+++ tests/dispatcher.js
@@ -1,28 +1,64 @@
 /* global describe, it*/
 var assert = require("assert"),
+    gamejs = require('gamejs'),
     gramework = require('../lib/gramework'),
     Dispatcher = gramework.Dispatcher;
 
 describe("Dispatcher", function() {
     it("should initialize dispatcher", function() {
-        var d = new Dispatcher({});
+        var d = new Dispatcher(gamejs);
         assert.deepEqual(d.stack, []);
     });
 
+    it("should allow extending", function() {
+        var MyDispatcher = Dispatcher.extend({
+            initialize: function() {
+                this.count = 0;
+            },
+
+            onTick: function(dt) {
+                this.count += 1;
+            }
+        });
+
+        var d = new MyDispatcher(gamejs);
+        assert.equal(d.count, 0);
+        d.onTick(1);
+        assert.equal(d.count, 1);
+    });
+
     it("should initialize with stack", function() {
-        var d = new Dispatcher({
+        var d = new Dispatcher(gamejs, {
             initial: 1
         });
         assert.deepEqual(d.stack, [1]);
     });
 
     it("should get top of stack", function() {
-        var d = new Dispatcher({
+        var d = new Dispatcher(gamejs, {
             initial: 1
         });
 
         d.push(2);
         assert.equal(d.top(), 2);
         assert.equal(d.parent(), 1);
+    });
+
+    it("should send update to top of stack", function() {
+        var ST = function() {
+            this.count = 0;
+        };
+        ST.prototype.update = function(dt) {
+            this.count += 1;
+        };
+
+        var st = new ST();
+        var d = new Dispatcher(gamejs, {
+            initial: st
+        });
+
+        assert.equal(st.count, 0);
+        d.update(1);
+        assert.equal(st.count, 1);
     });
 });
